29babeffca04e6583886f8301e0e7c1863bac960
Maha Dev <mahadev@nextgeni.com>
2021-01-08 19:08:08 +0500

    resource controllers teams and players

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Route::get('/', function () {
 Route::get('login', 'LoginController@login');
 Route::get('dashboard', 'DashboardController@dashboard');
 Route::get('dashboard1', 'DashboardController@dashboard1');
+
+Route::resource('/teams', 'TeamController');
+
+Route::resource('/players', 'PlayerController');
+
+
 $router->group(['middleware' => ['webAuth']], function () use ($router) {
     
 });
