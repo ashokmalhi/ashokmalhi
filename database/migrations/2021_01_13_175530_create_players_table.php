ddafc258871f9290fe58c692ae406cfb7c364d5b
Maha Dev <mahadev@nextgeni.com>
2021-04-19 16:17:49 +0500

    db migrations

--- database/migrations/2021_01_13_175530_create_players_table.php
+++ database/migrations/2021_01_13_175530_create_players_table.php
@@ -15,22 +15,14 @@ class CreatePlayersTable extends Migration
     {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->string('full_name')->nullable();
-            $table->string('email')->nullable();
+            $table->integer('fk_user')->nullable();
             $table->integer('player_no')->nullable();
-            $table->date('date_of_birth')->nullable();
-            $table->string('image_path', 200)->nullable();
-            $table->enum('gender', ['male', 'female'])->nullable();
             $table->float('height')->default(0);
             $table->float('weight')->default(0);
             $table->float('max_heart_rate')->default(0);
             $table->float('target_heart_rate')->default(0);
             $table->float('max_speed')->default(0);
             $table->boolean('track_heart_rate')->default(false);
-            $table->string('sensor_no')->nullable();
-            $table->string('position')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
